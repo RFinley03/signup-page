59c8c503f94872e44a7050742d33718ea8b292ec
Randy Finley <randy.finley03@gmail.com>
2022-07-30 23:38:16 -0400

    updated javascript file

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const elem = document.querySelector("#_email");
+const _email = document.querySelector("#_email");
 
 _email.addEventListener("focus", () => {
-    elem.placeholder = '';
+    _email.placeholder = '';
 })
 
 _email.addEventListener("blur", () => {
-    elem.placeholder = "eg: John_Doe@gmail.com";
+    _email.placeholder = "eg: John_Doe@gmail.com";
 })
