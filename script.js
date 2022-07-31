ba888cb075d02ebf609cd186b3bb6564929f729e
Randy Finley <randy.finley03@gmail.com>
2022-07-31 07:44:10 -0400

    update

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ _email.addEventListener("blur", () => {
     _email.placeholder = "eg: John_Doe@gmail.com";
 })
 
-submit.addEventListener("click", (e) => {
+submit.addEventListener("click", e => {
     e.preventDefault();
     if (_email.value === "" || _name === "") {
         let _li = document.createElement("li");
         _li.classList.add('alert');
         _li.appendChild(document.createTextNode(`* please enter all text fields`));
-        _ul.appendChild(_li);
+        _ul.prepend(_li);
         setTimeout(() => {
             _ul.removeChild(_li);
         }, 3000);
